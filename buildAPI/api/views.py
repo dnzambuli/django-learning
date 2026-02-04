1fb1e502950b125856e796569876f5e086996a61
dnzambuli <damunzandm@outlook.com>
2026-02-04 09:34:26 +0300

    generic views

--- buildAPI/api/views.py
+++ buildAPI/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskListCreate(generics.ListCreateAPIView):
+    """
+    Handles GET requests to list all tasks.
+    Handles POST requests to create new tasks.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Handles GET for one task, PUT/PATCH for updating, and DELETE to remove a task
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
