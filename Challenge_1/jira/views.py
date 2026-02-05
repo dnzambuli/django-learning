6b493e1a57bbdefa5077432065a5148db35c55e4
dnzambuli <damunzandm@outlook.com>
2026-02-05 15:56:07 +0300

    rendering the users

--- Challenge_1/jira/views.py
+++ Challenge_1/jira/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+
+def index(request):
+    users = User.objects.all()
+    return render(request, 'index.html', {"users": users})
+
+# from django.http import HttpResponse
+# from pathlib import Path
+#
+# def index(request):
+#     path = Path(__file__).resolve()
+#     return HttpResponse(f"VIEW FILE: {path}")
