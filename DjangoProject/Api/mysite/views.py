ac2976d32b81e2a4df3e11e7f873406d1f593ffe
dnzambuli <damunzandm@outlook.com>
2026-02-10 08:43:17 +0300

    the blogpost view

--- DjangoProject/Api/mysite/views.py
+++ DjangoProject/Api/mysite/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+# django rest frameworks to provide default views
+from rest_framework.views import generics
+
+# import the model made
+from .models import BlogPost
+
+# import the model serializer
+from serializers import BlogPostSerializer
+
+
 # Create your views here.
+class BlogPostListView(generics.ListAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
