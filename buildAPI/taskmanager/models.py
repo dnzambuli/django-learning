325e876f5071069c89dc20350764dc38c60c4955
dnzambuli <damunzandm@outlook.com>
2026-02-13 16:09:22 +0300

    tried to implement dry code using settings variables

--- buildAPI/taskmanager/models.py
+++ buildAPI/taskmanager/models.py
@@ -1,21 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from core import settings # import the settings to access AUTH_USER_MODEL
+
 # Create your models here.
 class Task(models.Model):
-    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)  # link task to user
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # link task to user, because i changed the user model to a custom one
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def Meta(self):
+    class Meta:
         ordering = ['-created_at']  # newest tasks first
 
     def __str__(self):
         return self.title
     
-class CustomUser(User.AbstractUser):
+class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
