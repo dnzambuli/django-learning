a7e37733966acd77c6db06b13c74ab496b0f6f1b
dnzambuli <damunzandm@outlook.com>
2026-02-13 14:18:39 +0300

    json creators

---
+++ buildAPI/taskmanager/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Task, CustomUser
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__' # Get all fields from Task model
+        read_only_fields = ['user'] # make user field read-only since it will be set to the authenticated user in the view
+
+# class CustomUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['id', 'username', 'email', 'profile_picture'] # specify fields to include in the serializer
