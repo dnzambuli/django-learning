7ac400a4ffd3cee9c327797df458004fc5f2f619
dnzambuli <damunzandm@outlook.com>
2026-02-13 16:10:15 +0300

    removed the user serializer to simplify the apis

--- buildAPI/taskmanager/serializers.py
+++ buildAPI/taskmanager/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Task, CustomUser
+from .models import Task# , CustomUser
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
